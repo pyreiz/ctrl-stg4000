679dc612ae4a7a1193632ee5e9e0f1131ba46a55
agricolab <robertsadresse@gmx.de>
2019-12-16 08:59:24 +0100

    Remove deprecated download_smart

--- stg/wrapper.py
+++ stg/wrapper.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 import clr
 from pathlib import Path
 import System
+
 libpath = Path(__file__).parent.parent
 dllpath = libpath / "bin" / "McsUsbNet.dll"
 fullPath = str(dllpath)
@@ -15,66 +16,65 @@ from Mcs.Usb import *
 from functools import lru_cache
 from time import sleep
 from typing import List
+
 # %%
-def available(serial:int=None):
+def available(serial: int = None):
     deviceList = CMcsUsbListNet()
     deviceList.Initialize(DeviceEnumNet.MCS_STG_DEVICE)
-    if serial is None: #return a list of all available
+    if serial is None:  # return a list of all available
         devices = []
         for dev_num in range(0, deviceList.GetNumberOfDevices()):
-            devices.append(deviceList.GetUsbListEntry(dev_num))            
+            devices.append(deviceList.GetUsbListEntry(dev_num))
         return devices
-        
-    else: #return the device with the desired serial number
+
+    else:  # return the device with the desired serial number
         device = None
-        for dev_num in range(0, deviceList.GetNumberOfDevices()):        
+        for dev_num in range(0, deviceList.GetNumberOfDevices()):
             ser = int(deviceList.GetUsbListEntry(dev_num).SerialNumber)
             if ser == serial:
-                device = deviceList.GetUsbListEntry(dev_num)            
+                device = deviceList.GetUsbListEntry(dev_num)
         return device
-       
-        
-class BasicInterface():
-    
+
+
+class BasicInterface:
     def __enter__(self):
         self.connect()
         return self
-    
+
     def __exit__(self, type, value, tb):
-        self.disconnect()        
-        
+        self.disconnect()
+
     def __init__(self, info):
         self._info = info
         self._interface = CStg200xDownloadNet()
-        
+
     def connect(self):
         self.connected = True
         self._interface.Connect(self._info)
-        
+
     def disconnect(self):
         self.connected = False
         self._interface.Disconnect()
-        
+
     def __getattr__(self, item):
         return getattr(self._interface, item)
 
 
-def bitmap(valuelist:list):
-    bmap = sum(map(lambda x: 2**x, valuelist)) 
+def bitmap(valuelist: list):
+    bmap = sum(map(lambda x: 2 ** x, valuelist))
     return None if bmap == 0 else bmap
-            
-            
-class STG4000():
 
-    def sleep(self, duration:float):
+
+class STG4000:
+    def sleep(self, duration: float):
         sleep(duration)
-        
-    def __init__(self, serial:int=None):
+
+    def __init__(self, serial: int = None):
         if serial is None:
             info = available()[0]
         else:
             info = available(serial)
-        print('Selecting {0:s}:SN {1:s}'.format(info.DeviceName, info.SerialNumber))            
+        print("Selecting {0:s}:SN {1:s}".format(info.DeviceName, info.SerialNumber))
         self._info = info
         self.interface = lambda: BasicInterface(info)
         self.diagonalize_triggermap()
@@ -83,18 +83,19 @@ class STG4000():
     def name(self):
         "returns the model name, i.e. STG4002/4/8"
         return self._info.DeviceName
-    
+
     @property
-    def version(self):    
+    def version(self):
         "Returns the current hardware and software version"
         soft, hard = self._version
-        return 'Hardware - {0} : Software - Version: {1}'.format(
-                hard.replace('Rev.', 'Revision'),
-                soft)
+        return "Hardware - {0} : Software - Version: {1}".format(
+            hard.replace("Rev.", "Revision"), soft
+        )
+
     @property
     def _version(self):
         with self.interface() as interface:
-            _, soft, hard = interface.GetStgVersionInfo("","")
+            _, soft, hard = interface.GetStgVersionInfo("", "")
         return soft, hard
 
     @property
@@ -106,14 +107,14 @@ class STG4000():
     def manufacturer(self):
         "Returns the name of the manufacturer"
         return self._info.Manufacturer
-    
+
     def __repr__(self):
         return f"{str(self)} at {hex(id(self))}"
-    
+
     def __str__(self):
         return self._info.ToString()
 
-    def set_current_mode(self, channel_index:int=all):   
+    def set_current_mode(self, channel_index: int = all):
         """set a single or all channels to current mode
         
         args
@@ -129,9 +130,8 @@ class STG4000():
         else:
             with self.interface() as interface:
                 interface.SetCurrentMode(System.UInt32(channel_index))
-        
-                
-    def set_voltage_mode(self, channel_index:int=all):
+
+    def set_voltage_mode(self, channel_index: int = all):
         """set a single or all channels to voltage mode
         
         args
@@ -148,77 +148,74 @@ class STG4000():
         else:
             with self.interface() as interface:
                 interface.SetVoltageMode(System.UInt32(channel_index))
-        
+
     @property
     def current_resolution_in_uA(self):
         with self.interface() as interface:
-            out = (interface.GetCurrentResolutionInNanoAmp(System.UInt32(0)) / 
-                   1000)
+            out = interface.GetCurrentResolutionInNanoAmp(System.UInt32(0)) / 1000
         return out
 
     @property
     def current_resolution_in_mA(self):
         with self.interface() as interface:
-            out = (interface.GetCurrentResolutionInNanoAmp(System.UInt32(0)) / 
-                   (1000*1000))
+            out = interface.GetCurrentResolutionInNanoAmp(System.UInt32(0)) / (
+                1000 * 1000
+            )
         return out
-    
+
     @property
     def current_range_in_mA(self):
         with self.interface() as interface:
-            out = (interface.GetCurrentRangeInNanoAmp(System.UInt32(0))/
-                   (1000*1000))
+            out = interface.GetCurrentRangeInNanoAmp(System.UInt32(0)) / (1000 * 1000)
         return out
 
     @property
     def current_range_in_uA(self):
         with self.interface() as interface:
-            out = (interface.GetCurrentRangeInNanoAmp(System.UInt32(0))/
-                   (1000))
+            out = interface.GetCurrentRangeInNanoAmp(System.UInt32(0)) / (1000)
         return out
 
     @property
     def voltage_resolution_in_uV(self):
         with self.interface() as interface:
-            out = (interface.GetVoltageResolutionInMicroVolt(System.UInt32(0)))
-        return out    
-        
+            out = interface.GetVoltageResolutionInMicroVolt(System.UInt32(0))
+        return out
+
     @property
     def voltage_range_in_mV(self):
         with self.interface() as interface:
-            out = (interface.GetVoltageRangeInMicroVolt(System.UInt32(0)) / 
-                 (1000))
+            out = interface.GetVoltageRangeInMicroVolt(System.UInt32(0)) / (1000)
         return out
-    
+
     @property
     def time_resolution_in_us(self):
         return 20
-    
+
     @property
     def time_resolution_in_ms(self):
         return 0.02
-    
+
     @property
     def DAC_resolution(self):
         with self.interface() as interface:
             out = interface.GetDACResolution()
         return out
-        
+
     @property
     def channel_count(self):
         "returns the number of stimulation channels"
         with self.interface() as interface:
             out = interface.GetNumberOfAnalogChannels()
         return out
-    
+
     @property
     def trigin_count(self):
         "returns the number of  trigger inputs"
         with self.interface() as interface:
-            out =  interface.GetNumberOfTriggerInputs()
+            out = interface.GetNumberOfTriggerInputs()
         return out
-    
-    def stop_stimulation(self, targets:list=all):   
+
+    def stop_stimulation(self, targets: list = all):
         """stops all trigger inputs or a selection based on a list 
         
         Triggers are mapped to channels according to the channelmap.
@@ -235,13 +232,13 @@ class STG4000():
             Indexing starts at 0.
         
         """
-        
+
         if targets is all:
             targets = [c for c in range(self.channel_count)]
         with self.interface() as interface:
             interface.SendStop(System.UInt32(bitmap(targets)))
-        
-    def start_stimulation(self, channels:list=all):
+
+    def start_stimulation(self, channels: list = all):
         """starts all trigger inputs or a selection based on a list 
         
         Triggers are mapped to channels according to the channelmap.
@@ -258,9 +255,9 @@ class STG4000():
             Indexing starts at 0.
         
         """
-        
+
         if channels is all:
-            channels= [c for c in range(self.channel_count)]
+            channels = [c for c in range(self.channel_count)]
         with self.interface() as interface:
             interface.SendStart(System.UInt32(bitmap(channels)))
 
@@ -278,23 +275,19 @@ class STG4000():
         syncoutmap = []
         repeat = []
         for chan_idx in range(0, self.channel_count):
-            repeat.append(1) # every trigger only once
-            syncoutmap.append(1 <<chan_idx) #diagonal triggerout
-            channelmap.append(1 <<chan_idx) #diagonal triggerin
+            repeat.append(1)  # every trigger only once
+            syncoutmap.append(1 << chan_idx)  # diagonal triggerout
+            channelmap.append(1 << chan_idx)  # diagonal triggerin
         with self.interface() as interface:
-            interface.SetupTrigger(0, channelmap, syncoutmap, repeat);        
-    
-    def download_smart(self, channel_index:int=0,
-                       amplitudes_in_mA:List[float, ]=[0],
-                       durations_in_ms:List[float, ]=[0]):
+            interface.SetupTrigger(0, channelmap, syncoutmap, repeat)
 
-        print("Deprecated, use download instead")        
-        self.download(channel_index, amplitudes_in_mA, durations_in_ms)        
-    
-    def download(self, channel_index:int=0,
-                 amplitudes_in_mA:List[float, ]=[0],
-                 durations_in_ms:List[float, ]=[0]):
-        
+    def download(
+        self,
+        channel_index: int = 0,
+        amplitudes_in_mA: List[float,] = [0],
+        durations_in_ms: List[float,] = [0],
+    ):
+
         """Download a stimulation signal 
         
         The signal is compressed to amplitudes and their respective durations 
@@ -323,30 +316,40 @@ class STG4000():
            The signal is downloaded with  interface.PrepareAndSendData, 
            therefore previous data sent to that channel is erased first.
         """
-        
-        amplitudes = [int(a*1000_000) for a in amplitudes_in_mA]
-        durations = [int(s*1000) for s in durations_in_ms]
+
+        amplitudes = [int(a * 1000_000) for a in amplitudes_in_mA]
+        durations = [int(s * 1000) for s in durations_in_ms]
         self._download(channel_index, amplitudes, durations, "current")
-        
-    
-    def _download(self, channel_index:List[int,]=0, amplitude:List[int,]=[0], 
-                 duration:list=[20], mode='current'):
-     
+
+    def _download(
+        self,
+        channel_index: List[int,] = 0,
+        amplitude: List[int,] = [0],
+        duration: list = [20],
+        mode="current",
+    ):
+
         if len(amplitude) != len(duration):
             raise ValueError("Every amplitude needs a duration and vice versa!")
-        amplitude = ([System.Int32(a) for a in amplitude])
-        duration  = ([System.UInt64(d) for d in duration])
+        amplitude = [System.Int32(a) for a in amplitude]
+        duration = [System.UInt64(d) for d in duration]
 
         if mode == "current":
             self.set_current_mode(channel_index)
-            with self.interface() as interface:           
-                interface.PrepareAndSendData(channel_index, amplitude, duration,
-                                             STG_DestinationEnumNet.channeldata_current)
+            with self.interface() as interface:
+                interface.PrepareAndSendData(
+                    channel_index,
+                    amplitude,
+                    duration,
+                    STG_DestinationEnumNet.channeldata_current,
+                )
         elif mode == "voltage":
             self.set_voltage_mode(System.UInt32(channel_index))
-            with self.interface() as interface:           
-                interface.PrepareAndSendData(channel_index, amplitude, duration,
-                                             STG_DestinationEnumNet.channeldata_voltage)
-        
-
+            with self.interface() as interface:
+                interface.PrepareAndSendData(
+                    channel_index,
+                    amplitude,
+                    duration,
+                    STG_DestinationEnumNet.channeldata_voltage,
+                )
 
