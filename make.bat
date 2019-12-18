1cccce2f5c0b0094ef5d7418612e3d7ec6dfd578
agricolab <robertsadresse@gmx.de>
2019-12-18 11:05:21 +0100

    Run all tests on win with make test

--- make.bat
+++ make.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
+REM Command file for Sphinx documentation
+
+echo %0 %1
 if "%1" == "" goto help
 
 if "%1" == "help" (
@@ -6,5 +9,15 @@ if "%1" == "help" (
     echo.Please use `make ^<target^>` where ^<target^> is one of
 	echo.  test       to run pytest, mpyp and coverage html
 	goto end
-
+)
+if "%1" == "test" (
+	:test
+	echo.  run pytest %2
+    pytest %2
+    echo.  run mypy
+    mypy
+    echo.  run coverage html
+    coverage html    
+	goto end
+)
 :end
